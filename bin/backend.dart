72d5413da2ac55d320aa3ddf17ab602858b6607e
Johnny Suzuki <johnnywaklersilva@gmail.com>
2022-08-02 17:45:17 -0300

    Aula 3

---
+++ bin/backend.dart
@@ -0,0 +1,8 @@
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart' as shelf_io;
+
+void main() async {
+  final server = await shelf_io.serve(
+      (request) => Response(200, body: 'status 200 ok'), 'localhost', 8080);
+  print("Servidor inicado em http://localhost:8080");
+}
