e2c8b684fc61c1d29a5882b77feb2aa093ca9c44
Johnny Suzuki <johnnywaklersilva@gmail.com>
2022-08-26 21:24:15 -0300

    Aula 6 - Metodo POST e JSON para login

--- bin/server_handler.dart
+++ bin/server_handler.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
 
@@ -8,7 +10,7 @@ class ServeHandler {
     router.get('/', (Request request) {
       return Response(200, body: 'Primeira rota');
     });
-
+    // Aula 5 - Usando Get
     // http:/localhost:8080/ola/mundo
     // Retorna 'Ola, mundo'
 
@@ -22,6 +24,22 @@ class ServeHandler {
       String? nome = req.url.queryParameters['nome'];
       String? idade = req.url.queryParameters['idade'];
       return Response.ok('Query é: nome "$nome", idade: "$idade"');
+    });
+
+    // Aula 6 - trabalhando com POST e JSON
+    router.post('/login', (Request req) async {
+      var result = await req.readAsString();
+      Map json = jsonDecode(result);
+
+      var usuario = json['usuario'];
+      var senha = json['senha'];
+
+      //Se usuário == admin e senha == 123
+      if (usuario == 'admin' && senha == '123') {
+        return Response.ok('Bem vindo, $usuario');
+      } else {
+        return Response.forbidden('Acesso negado');
+      }
     });
 
     return router;
